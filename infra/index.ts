6c4c9b3959714597dd0d32a4dea8936c9e9ac7f0
Anish Pallati <anishp@duck.com>
2024-10-15 15:56:30 -0400

    feat: create userpool for hackathon hosts

--- infra/index.ts
+++ infra/index.ts
@@ -1,2 +1,18 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
+// Create a UserPool and client for hackathon hosts
+const hostUserPool = new aws.cognito.UserPool("hackathon-hosts", {
+    userPoolAddOns: {
+        advancedSecurityMode: "ENFORCED",
+    },
+    usernameAttributes: ["email"],
+    autoVerifiedAttributes: ["email"],
+});
+
+const hostUserPoolClient = new aws.cognito.UserPoolClient("hackathon-hosts-client", {
+    userPoolId: hostUserPool.id,
+    generateSecret: false,
+});
+
+export const hostUserPoolId = hostUserPool.id;
+export const hostUserPoolClientId = hostUserPoolClient.id;
