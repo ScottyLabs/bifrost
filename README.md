0574c05c10556a3b1cf4b2229504e1d19c35c444
Anish Pallati <anishp@duck.com>
2024-10-15 16:16:42 -0400

    style: run prettier format

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Centralized monorepo for collegiate hackathon backend, registration, dashoard, and judging systems.
 
-## Installation 
+## Installation
 
 This project uses [Node.js](https://nodejs.org/en). We recommend using [NVM](https://nodejs.org/en/download/package-manager/) (Node Version Manager) to install it.
 
--- infra/index.ts
+++ infra/index.ts
@@ -2,17 +2,20 @@ import * as aws from "@pulumi/aws";
 
 // Create a UserPool and client for hackathon hosts
 const hostUserPool = new aws.cognito.UserPool("hackathon-hosts", {
-    userPoolAddOns: {
-        advancedSecurityMode: "ENFORCED",
-    },
-    usernameAttributes: ["email"],
-    autoVerifiedAttributes: ["email"],
+  userPoolAddOns: {
+    advancedSecurityMode: "ENFORCED",
+  },
+  usernameAttributes: ["email"],
+  autoVerifiedAttributes: ["email"],
 });
 
-const hostUserPoolClient = new aws.cognito.UserPoolClient("hackathon-hosts-client", {
+const hostUserPoolClient = new aws.cognito.UserPoolClient(
+  "hackathon-hosts-client",
+  {
     userPoolId: hostUserPool.id,
     generateSecret: false,
-});
+  },
+);
 
 export const hostUserPoolId = hostUserPool.id;
 export const hostUserPoolClientId = hostUserPoolClient.id;
