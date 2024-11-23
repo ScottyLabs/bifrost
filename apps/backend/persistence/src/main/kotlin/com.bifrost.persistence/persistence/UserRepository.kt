4ff13789add608dc5df46ecd5ed75a0ba32de5ee
Arav Bhardwaj <aravb09@gmail.com>
2024-11-23 16:59:47 -0500

    refactor: move files to persistence

---
+++ apps/backend/persistence/src/main/kotlin/com.bifrost.persistence/persistence/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.scottylabs.resource.repository
+
+
+import com.scottylabs.resource.domain.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface UserRepository : JpaRepository<User, UUID> {
+  fun findByEmail(email: String): User?
+}
