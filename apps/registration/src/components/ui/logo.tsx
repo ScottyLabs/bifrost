942b1a3bc1f74d5513817dda6cfd9667c83d7703
Gabriel Hall <adrastopoulos@gmail.com>
2024-12-04 06:28:58 -0500

    feat: remove registration app

--- apps/registration/src/components/ui/logo.tsx
+++
@@ -1,7 +0,0 @@
-export function Logo({ className }: { className?: string }) {
-  return (
-    <div className={className}>
-      <img src="/logo.svg" alt="Bifrost Logo" className="h-full w-auto" />
-    </div>
-  );
-}
