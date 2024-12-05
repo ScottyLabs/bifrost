c8f5022a60c347cf3131467045d383e9893ecbea
Xavier Lien <xavilien@gmail.com>
2024-12-05 10:11:52 -0500

    feat: toggle menu

--- apps/landing/src/components/header.tsx
+++ apps/landing/src/components/header.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function HeaderLink({
   name,
   href,
   underlineColor,
+  onClick,
 }: {
   name: string;
   href: string;
   underlineColor: string;
+  onClick: () => void;
 }) {
   return (
     <a href={href}>
       <span
         className={`cursor-pointer hover:underline ${underlineColor} decoration-8 underline-offset-8`}
+        onClick={onClick}
       >
         {name}
       </span>
@@ -21,49 +25,68 @@ function HeaderLink({
 }
 
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="sticky top-0 w-full pt-6 pb-4 hidden lg:block backdrop-blur z-40">
+    <header className="sticky top-0 w-full pt-6 pb-4 backdrop-blur z-40">
       <div className="flex flex-row justify-between text-xl mr-12 ml-8">
-        <div className="cursor-pointer">
+        <div className="cursor-pointer w-1/6">
           <a href="/">
             <img src="/logo.svg" alt="logo" />
           </a>
         </div>
-        <div className="space-x-16 text-lg flex items-center">
+        <button
+          className="lg:hidden text-white text-3xl md:text-5xl"
+          onClick={toggleMenu}
+        >
+          ☰
+        </button>
+        <div className={`flex absolute right-6 md:right-8 lg:right-0 top-20 md:top-24 space-y-2 lg:space-y-0 lg:top-0 lg:relative lg:space-x-16 text-lg flex-col justify-center lg:flex-row items-center lg:flex ${isMenuOpen ? "block" : "hidden"}`}>
           <HeaderLink
             name="About"
             href="#about"
             underlineColor="decoration-yellow"
+            onClick={toggleMenu}
           />
           <HeaderLink
             name="Theme"
             href="#theme"
             underlineColor="decoration-pink"
+            onClick={toggleMenu}
           />
           <HeaderLink
             name="Schedule"
             href="#schedule"
             underlineColor="decoration-blue"
+            onClick={toggleMenu}
           />
           <HeaderLink
             name="Speakers"
             href="#speakers"
             underlineColor="decoration-purple"
+            onClick={toggleMenu}
           />
           <HeaderLink
             name="Prizes"
             href="#prizes"
             underlineColor="decoration-blue"
-          />
-          <HeaderLink
-            name="Sponsors"
-            href="#sponsors"
-            underlineColor="decoration-yellow"
+            onClick={toggleMenu}
           />
           <HeaderLink
             name="FAQs"
             href="#faq"
             underlineColor="decoration-pink"
+            onClick={toggleMenu}
+          />
+          <HeaderLink
+            name="Sponsors"
+            href="#sponsors"
+            underlineColor="decoration-yellow"
+            onClick={toggleMenu}
           />
         </div>
       </div>
