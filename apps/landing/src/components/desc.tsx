bbf0b26c083b43603fa8a60708f6908cc7ccced5
Xavier Lien <xavilien@gmail.com>
2024-12-04 23:55:14 -0500

    feat: description section

--- apps/landing/src/components/desc.tsx
+++ apps/landing/src/components/desc.tsx
@@ -1,12 +1,19 @@
 export function Desc() {
   return (
-    <section className="min-h-screen w-full items-center justify-center">
-      <div className="text-white text-center font-thin font-sfpro">
-        <p className="text-xl">
-          <p className="uppercase font-basteleur">Tartanhacks</p>
-          is the TartanHacks is the largest Hackathon in Pittsburgh! Organized by ScottyLabs, it’s a 24-hour hackathon where participants from all over the country create innovative projects. This year, we are happy to be fully in person again! Come on over to hack, learn from our workshops and incredible speakers, and meet other hackers from: Feb 7-8 @ Carnegie Mellon University!
+    <section className="flex flex-col justify-center items-center">
+      <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0" />
+      <div className="text-white text-center text-xl font-sfpro w-2/3 md:w-1/2">
+        <p>
+          <span className="uppercase font-basteleur">Tartanhacks </span>
+          is the largest Hackathon in Pittsburgh! Organized by ScottyLabs, it’s a 24-hour hackathon where participants
+          from all over the country create innovative projects. This year, we are happy to be fully in person again!
+          Come on over to hack, learn from our workshops and incredible speakers, and meet other hackers from:
+        </p>
+        <p className="mt-5">
+          Feb 7-8 @ Carnegie Mellon University!
         </p>
       </div>
+      <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0" />
     </section>
   );
 }
