89e6b6b9174900ae847ec590a242c7ce0c70d92d
Xavier Lien <xavilien@gmail.com>
2024-12-04 23:40:37 -0500

    style: change to tartanhacks svg

--- apps/landing/src/components/hero.tsx
+++ apps/landing/src/components/hero.tsx
@@ -4,9 +4,7 @@ export function Hero() {
   return (
     <section className="min-h-screen w-full flex items-center justify-center flex-col">
       <div>
-        <h1 className="uppercase font-extrabold text-6xl sm:text-8xl md:text-9xl font-basteleur bg-clip-text text-center">
-          Tartanhacks 2025
-        </h1>
+        <img src="/tartanhacks.svg" alt="logo" className="px-10" />
       </div>
       <div className="text-white text-center mt-8 font-thin font-futura">
         <h2 className="text-2xl">Presented by CMU ScottyLabs</h2>
