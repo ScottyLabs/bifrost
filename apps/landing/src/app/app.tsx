ebabdc53642d5f1214a565cdb61c2bdff2c4e8f0
Xavier Lien <xavilien@gmail.com>
2024-12-05 00:15:01 -0500

    feat: prizes

--- apps/landing/src/app/app.tsx
+++ apps/landing/src/app/app.tsx
@@ -4,6 +4,7 @@ import { Theme } from "../components/theme";
 import { Desc } from "../components/desc";
 import { Schedule } from "../components/schedule";
 import { Speakers } from "../components/speakers";
+import { Prizes } from "../components/prizes";
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
       <Theme />
       <Schedule />
       <Speakers />
+      <Prizes />
     </div>
   );
 }
