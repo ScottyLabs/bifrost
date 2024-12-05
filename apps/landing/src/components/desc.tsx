58b7d7ebc704e183c0b67afce75e91db0d1ca93c
Xavier Lien <xavilien@gmail.com>
2024-12-05 00:57:44 -0500

    style: got swirl to work

--- apps/landing/src/components/desc.tsx
+++ apps/landing/src/components/desc.tsx
@@ -4,8 +4,8 @@ export function Desc() {
       className="flex flex-col justify-center items-center py-36 mb-36"
       id="desc"
     >
-      <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0" />
-      <div className="text-white text-center text-xl font-sfpro w-2/3 md:w-1/2">
+      <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0 z-40" />
+      <div className="text-white text-center text-xl font-sfpro w-2/3 md:w-1/2 z-40">
         <p>
           <span className="uppercase font-basteleur">Tartanhacks </span>
           is the largest Hackathon in Pittsburgh! Organized by ScottyLabs, it’s a 24-hour hackathon where participants
@@ -16,7 +16,7 @@ export function Desc() {
           Feb 7-8 @ Carnegie Mellon University!
         </p>
       </div>
-      <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0" />
+      <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0 z-40" />
     </section>
   );
 }
