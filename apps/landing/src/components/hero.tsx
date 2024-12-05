37ff210da10494e4a226160897e535dd7aa448f0
Xavier Lien <xavilien@gmail.com>
2024-12-05 01:18:07 -0500

    style: change z-indexes

--- apps/landing/src/components/hero.tsx
+++ apps/landing/src/components/hero.tsx
@@ -6,10 +6,10 @@ export function Hero() {
       className="min-h-screen w-full flex items-center justify-center flex-col"
       id="about"
     >
-      <div>
+      <div className="z-40">
         <img src="/tartanhacks.svg" alt="tartanhacks" className="px-10" />
       </div>
-      <div className="text-white text-center mt-8 font-thin font-futura">
+      <div className="text-white text-center mt-8 font-thin font-futura z-40">
         <h2 className="text-2xl">Presented by CMU ScottyLabs</h2>
         <p className="text-xl">Feb 7 — 8, 2025</p>
       </div>
