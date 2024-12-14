b995e341f7813dc551ae1285a7f2c0fbe88fe0fe
Gabriel Hall <76796897+Adrastopoulos@users.noreply.github.com>
2024-12-14 05:59:00 -0800

    Update init-db.sh

--- apps/backend/init-db.sh
+++ apps/backend/init-db.sh
@@ -4,6 +4,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE resource;
-    CREATE DATABASE auth;
     GRANT ALL PRIVILEGES ON DATABASE resource TO bifrost_user;
 EOSQL
