65e7e5fa67e8898d4adb12720fd412cd7656a89e
Gabriel Hall <adrastopoulos@gmail.com>
2024-12-17 14:56:07 -0500

    feat: error boundary

--- apps/dashboard/app/root.tsx
+++ apps/dashboard/app/root.tsx
@@ -2,12 +2,14 @@ import "~/styles/index.css";
 
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import {
+  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useRouteError,
 } from "@remix-run/react";
 
 import { getSession } from "./services/session.server";
@@ -65,6 +67,66 @@ export default function App() {
       <Layout user={data.info}>
         <Outlet />
       </Layout>
+    </Document>
+  );
+}
+
+import React from "react";
+import type { ErrorResponse } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+interface GlobalNotFoundProps {
+  error: ErrorResponse;
+}
+
+export const GlobalNotFound: React.FC<GlobalNotFoundProps> = ({ error }) => {
+  return (
+    <section className="w-full min-h-screen flex items-center justify-center flex-col">
+      <h2 className="text-6xl font-bold">{error.status}</h2>
+      <p className="font-medium text-xl">Ooops!</p>
+      <h1 className="font-medium text-xl">Page Not Found</h1>
+      <Link to="/" className="mt-1 text-foreground font-bold">
+        Back to homepage
+      </Link>
+    </section>
+  );
+};
+
+interface GlobalRuntimeProps {
+  error: unknown;
+}
+
+export const GlobalRuntime: React.FC<GlobalRuntimeProps> = ({ error }) => {
+  const status = isRouteErrorResponse(error) ? error.status : 500;
+
+  return (
+    <section className="w-full min-h-screen flex items-center justify-center flex-col">
+      <h2 className="text-6xl font-bold">{status}</h2>
+      <p className="font-medium text-xl">Ooops!</p>
+      <h1 className="font-medium text-xl">Internal server error</h1>
+      <p className="text-sm">
+        Please try again later or feel free to contact us if the problem
+        persists
+      </p>
+    </section>
+  );
+};
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404)
+      return (
+        <Document>
+          <GlobalNotFound error={error} />
+        </Document>
+      );
+  }
+
+  return (
+    <Document>
+      <GlobalRuntime error={error} />
     </Document>
   );
 }
