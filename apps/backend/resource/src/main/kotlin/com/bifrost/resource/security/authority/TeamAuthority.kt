59a23a83489ce4dca91707b4de3abe1360b2c33e
Gabriel Hall <adrastopoulos@gmail.com>
2024-12-17 11:30:28 -0500

    chore: remove unused backend authority

--- apps/backend/resource/src/main/kotlin/com/bifrost/resource/security/authority/TeamAuthority.kt
+++
@@ -1,9 +0,0 @@
-package com.bifrost.resource.security.authority
-
-object TeamAuthority {
-  const val READ = "TEAM_READ"
-  const val CREATE = "TEAM_CREATE"
-  const val UPDATE = "TEAM_UPDATE"
-  const val DELETE = "TEAM_DELETE"
-  const val MANAGE_MEMBERS = "TEAM_MANAGE_MEMBERS"
-}
