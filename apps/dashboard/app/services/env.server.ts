80200a6560dc8c1614066af5b13afdb7dcacb0dc
Theodor Urban <theo.s.urban@gmail.com>
2024-12-17 12:43:03 -0500

    fix: Try hardcoding the vercel url

--- apps/dashboard/app/services/env.server.ts
+++ apps/dashboard/app/services/env.server.ts
@@ -6,6 +6,7 @@ const schema = z.object({
   OIDC_CLIENT_SECRET: z.string(),
   SESSION_SECRET: z.string(),
   API_URL: z.string(),
+  SELF_URI: z.string(),
 });
 
 export const env = schema.parse(process.env);
