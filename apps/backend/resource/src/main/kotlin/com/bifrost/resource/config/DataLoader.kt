8ed4eff5bcc6edfe360260ce55071e9a66765423
Gabriel Hall <adrastopoulos@gmail.com>
2024-12-17 10:40:22 -0500

    feat: add backend dataloader

---
+++ apps/backend/resource/src/main/kotlin/com/bifrost/resource/config/DataLoader.kt
@@ -0,0 +1,25 @@
+package com.bifrost.resource.config
+
+import com.bifrost.resource.model.User
+import com.bifrost.resource.repository.UserRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+
+@Component
+class DataLoader(
+  private val userRepository: UserRepository
+) {
+  @Bean
+  fun seedDatabase() = CommandLineRunner {
+    if (userRepository.count() == 0L) {
+      userRepository.saveAll(
+        listOf(
+          User(
+            externalId = "81decf9f-e1ce-4208-94aa-32c98e3eef1e",
+          ),
+        )
+      )
+    }
+  }
+}
