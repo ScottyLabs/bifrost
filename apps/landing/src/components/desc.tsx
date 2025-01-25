06a4ec4b3dca47fffa3a735defc999bcae97233e
Anish Pallati <anishp@duck.com>
2025-01-25 17:12:48 -0500

    feat: remove registration deadline countdown

--- apps/landing/src/components/desc.tsx
+++ apps/landing/src/components/desc.tsx
@@ -1,6 +1,3 @@
-import { DeadlineCountdown } from "./countdown";
-import { RegisterButton } from "./register";
-
 export function Desc() {
   return (
     <section
@@ -11,12 +8,12 @@ export function Desc() {
       <div className="text-white text-center text-xl font-sfpro w-2/3 md:w-1/2 z-30">
         <p>
           <span className="uppercase font-basteleur">Tartanhacks </span>
-          is the largest Hackathon in Pittsburgh! Organized by ScottyLabs, it’s a 24-hour in person hackathon where participants
-          from all over the country create innovative projects.
-          Come on over to hack, connect with peers, and learn from our incredible speakers. The application deadline is in
+          is the largest Hackathon in Pittsburgh! Organized by ScottyLabs, it's
+          a 24-hour in person hackathon where participants from all over the
+          country create innovative projects. Come on over to hack, connect with
+          peers, and learn from our incredible speakers. Registration is now
+          closed for all sstudents who are not actively enrolled at CMU.
         </p>
-        <DeadlineCountdown />
-        <RegisterButton />
       </div>
       <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0 z-30" />
     </section>
